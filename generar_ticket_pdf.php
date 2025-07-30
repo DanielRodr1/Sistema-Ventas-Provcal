e18e47e0a206ec3551207a6175989126e496cd87
DanielRodr1 <drodrigueza10@upao.edu.pe>
2025-07-30 10:11:44 -0500

    Modify Ticket - Printer v9

--- generar_ticket_pdf.php
+++ generar_ticket_pdf.php
@@ -21,82 +21,52 @@ $total = number_format($venta->total, 2);
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Ticket</title>
+    <title>Ticket Venta</title>
     <style>
-        * {
-            font-family: monospace;
-            font-size: 13px;
-        }
-
-        @media print {
-            @page {
-                size: 58mm auto;
-                margin: 0;
-            }
-
-            body {
-                margin: 0;
-            }
-        }
-
         body {
-            width: 58mm;
-            padding: 8px;
+            font-family: 'Courier New', monospace;
+            font-size: 13px;
+            width: 300px;
+            margin: 0 auto;
+            white-space: pre;
         }
-
-        .center {
+        .centrado {
             text-align: center;
         }
-
-        .left {
-            text-align: left;
-        }
-
-        .right {
-            text-align: right;
-        }
-
-        .spacer {
-            height: 10px;
-        }
-
-        pre {
-            font-family: monospace;
-            font-size: 13px;
-            margin: 0;
-            white-space: pre;
+        .linea {
+            margin: 8px 0;
         }
     </style>
 </head>
 <body onload="window.print(); setTimeout(() => window.location.href='vender.php', 1000);">
 
-<pre class="center" style="font-size:16px;">PROVCAL</pre>
-<pre class="center">Catering & Camps</pre>
+<div class="centrado">
+PROVCAL
+Catering & Camps
+</div>
 
-<div class="spacer"></div>
+Cliente: A.M.C  
+Fecha: <?= $fecha ?>            Hora: <?= $hora ?>
 
-<pre>Cliente: A.M.C</pre>
-<pre><?= str_pad("Fecha: " . $fecha, 24) . str_pad("Hora: " . $hora, 24, " ", STR_PAD_LEFT) ?></pre>
 
-<pre><?= str_repeat("=", 48) ?></pre>
-<pre><?= str_pad("Producto", 24) . str_pad("Cant", 6, " ", STR_PAD_LEFT) . str_pad("P.U.", 8, " ", STR_PAD_LEFT) . str_pad("Total", 10, " ", STR_PAD_LEFT) ?></pre>
-<pre><?= str_repeat("=", 48) ?></pre>
+<?= str_repeat("=", 48) . "\n" ?>
+<?= sprintf("%-24s%6s%8s%10s", "Producto", "Cant", "P.U.", "Total") . "\n" ?>
+<?= str_repeat("=", 48) . "\n" ?>
 
-<?php foreach ($productos as $producto): 
-    $nombre = mb_strimwidth($producto->nombre, 0, 24, "");
-    $cantidad = $producto->cantidad;
-    $precio = number_format($producto->precio, 2);
-    $subtotal = number_format($producto->precio * $producto->cantidad, 2);
-?>
-<pre><?= sprintf("%-24s%6s%8s%10s", $nombre, $cantidad, $precio, $subtotal) ?></pre>
-<?php endforeach; ?>
+<?php foreach ($productos as $prod):
+    $nombre = mb_strimwidth($prod->nombre, 0, 24, "");
+    $cantidad = $prod->cantidad;
+    $precio = number_format($prod->venta, 2);
+    $subtotal = number_format($prod->venta * $prod->cantidad, 2);
+    echo sprintf("%-24s%6s%8s%10s\n", $nombre, $cantidad, $precio, $subtotal);
+endforeach; ?>
 
-<pre><?= str_repeat("=", 48) ?></pre>
-<pre class="right">TOTAL: S/. <?= $total ?></pre>
+<?= str_repeat("=", 48) . "\n" ?>
+<?= str_pad("TOTAL: S/. " . $total, 48, " ", STR_PAD_LEFT) . "\n\n" ?>
 
-<div class="spacer"></div>
-<pre class="center">Gracias por su compra!</pre>
-<div class="spacer"></div>
+<div class="centrado">
+Gracias por su compra!
+</div>
 
 </body>
 </html>
